200c3119eec7ac4fad9d8ddcaa75765aef118643
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-29 01:57:47 +0900

    お試しnode

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Join from '../src/components/Join/ Join'
+import Chat from '../src/components/Chat/Chat'
+import {Route,Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = ()=>(
+  <Switch>
+     <Route path="/" exact component={Join} />
+    <Route psth='/chat' exact component={Chat} />
+  </Switch>
+);
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
+const http = require('http');
 const express = require('express');
-const socketio=require('socket.io');
-const http =require('http');
-const PORT =process.env.PORT||5000;
+const socketio = require('socket.io');
+const {addUser, removeUser, getUser,getUsersInRoom} = require('./users')
 const router=require('./router');
 const app =express();
 const server =http.createServer(app);
 const io =socketio(server) ;
+io.on('connect' ,(socket) =>{
+    socket.on('join',({name,room},callback) =>{
+       const {error,user} =addUser({id:socket.id,name,room});
+       if(error) return callback(error);
+       socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+        socket.broadcast.to(user.room).emit('message' ,{user:'admin' ,text:`${user.name}has joined`})
+       socket.join(user.room)
+       callback();
+    });
+    socket.on('sendMessage',(message,callback)=>{
+        const user=getUser(socket.id);
+        io.to(user.room).emit('message',{user:user.name,text:message});
+        callback();
+    })
+    socket.on('disconnect' , ()=>{
+        console.log('User had left!!!')
+    })
+})
 app.use(router);
-server.listen(PORT,()=>console.log(`Server has started on port ${PORT}`))
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
