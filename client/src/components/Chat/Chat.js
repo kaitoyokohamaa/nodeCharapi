f52bb6d340099dd2f628f0850582689292f61092
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-29 17:04:12 +0900

    エラーを出すため一旦コミット

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,119 +1,58 @@
-// import React, { useState, useEffect } from "react";
-// import queryString from 'query-string';
-// import io from "socket.io-client";
-// let socket;
-// const Chat=(location)=>{
-//     const [name,setName] =useState('');
-//     const [room,setRoom]=useState('');
-//     const [message,setMessage]=useState('');
-//     const [messages,setMessages]=useState([]);
-//     const ENDPOINT='localhost:5000'
-//      useEffect(() =>{
-//         const {name,room}=queryString.parse(location.search);
-//         socket=io(ENDPOINT);
-//     setName(name);
-//     setRoom(room);
-//     socket.emit('join' ,{name,room},(error)=>{
-//   if(error){
-//       alert(error)
-//      }
-//     });
-
-//     return()=>{
-//         socket.emit('disconnect');
-
-//         socket.off();
-//     }
-//     },[ENDPOINT,location.search]);
-//     useEffect(() =>{
-//         socket.on('message' ,message  =>{
-//             setMessages([...messages,message])
-//         })
-//     })
-
-
-//     const sendMessage=(event) =>{
-//         event.preventDefault();
-//         if(message){
-//             socket.emit('sendMessage' ,message,()=>setMessage(''));
-//         }
-//     }
-// console.log(message,messages)
-//     return(
-//         <div className="outerContainer">
-//            <div className="container">
-//                 <input 
-//                  value={message}
-//                　onChange={(event) => setMessage(event.target.value)}
-//                 onKeyPress={event =>event.key === 'Enter' ? sendMessage(event) :null}
-//                />
-//            </div>
-//         </div>
-//     )
-// }
-// export default Chat;
-
-import React, { useState, useEffect } from "react";
-import queryString from 'query-string';
+import React, { useState,useEffect } from 'react';
+import queryString from "query-string";
 import io from "socket.io-client";
-
-
-
-import './Chat.css';
-
 let socket;
-
-const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
-
-  useEffect(() => {
-    const { name, room } = queryString.parse(location.search);
-
-    socket = io(ENDPOINT);
-
+const Chat=(location)=>{
+    const [name,setName] =useState('');
+    const [room,setRoom]=useState('');
+    const [message,setMessage]=useState('');
+    const [messages,setMessages]=useState([]);
+    const ENDPOINT='localhost:5000'
+     useEffect(() =>{
+        const {name,room}=queryString.parse(location.search);
+        socket=io(ENDPOINT);
+        console.log(name);
+        console.log(room);
+    setName(name);
     setRoom(room);
-    setName(name)
-
-    socket.emit('join', { name, room }, (error) => {
-      if(error) {
-        alert(error);
-      }
+    socket.emit('join' ,{name,room},(error)=>{
+  if(error){
+      alert(error)
+     }
     });
-  }, [ENDPOINT, location.search]);
-  
-  useEffect(() => {
-    socket.on('message', message => {
-      setMessages(messages => [ ...messages, message ]);
-    });
-    
-    socket.on("roomData", ({ users }) => {
-      setUsers(users);
-    });
-}, []);
 
-  const sendMessage = (event) => {
-    event.preventDefault();
+    return()=>{
+        socket.emit('disconnect');
 
-    if(message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+        socket.off();
     }
-  }
-  console.log(message,messages)
-  return(
-    <div className="outerContainer">
-       <div className="container">
-            <input 
-             value={message}
-           　onChange={(event) => setMessage(event.target.value)}
-            onKeyPress={event =>event.key === 'Enter' ? sendMessage(event) :null}
-           />
-       </div>
-    </div>
-)
+    },[ENDPOINT,location.search]);
+
+    useEffect(() =>{
+        socket.on('message' ,message  =>{
+            setMessages([...messages,message])
+        })
+    },[messages])
+
+
+    const sendMessage=(event) =>{
+        event.preventDefault();
+        if(message){
+            socket.emit('sendMessage' ,message,()=>setMessage(''));
+        }
+    }
+console.log(message,messages)
+    return(
+        <div className="outerContainer">
+           <div className="container">
+                <input 
+                 value={message}
+               　onChange={(event) => setMessage(event.target.value)}
+                onKeyPress={event =>event.key === 'Enter' ? sendMessage(event) :null}
+               />
+           </div>
+        </div>
+    )
 }
 export default Chat;
+
